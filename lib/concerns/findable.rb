fa35776687b92cf7446dbeaf11d4fe44dc1b2b47
Baktiar Miah <baktiar.m@gmail.com>
2019-10-02 18:57:13 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,4 +1,9 @@
 module Findable 
     def self.find_by_name(name)
     @@artists.detect{|a| a.name == name}
+    
+    def self.find_by_name(name)
+    @@songs.detect{|a| a.name == name}
+  end
+    
   end
